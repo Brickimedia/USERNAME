aa15b314e933f952788a44a2c0c988756351b333
UltrasonicNXT <adamr_carter@btinternet.com>
2014-02-03 16:20:44 +0000

    Fix. Now working.

--- USERNAME.php
+++ USERNAME.php
@@ -10,7 +10,8 @@ $wgExtensionCredits['variable'][] = array(
 
 $wgExtensionMessagesFiles['USERNAME'] = __DIR__ . '/USERNAME.i18n.magic.php';
 
-$wgAutoLoadClasses['USERNAME'] = __DIR__ . '/USERNAME.hooks.php';
+//$wgAutoLoadClasses['USERNAME'] = __DIR__ . '/USERNAME.hooks.php'; doesn't work in time... :/
+require_once( __DIR__ . '/USERNAME.hooks.php' );
 
 $wgHooks['MagicWordwgVariableIDs'][] = 'USERNAME::setup';
 $wgHooks['ParserGetVariableValueSwitch'][] = 'USERNAME::run';
